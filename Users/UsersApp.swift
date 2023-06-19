38a2539528d5fe72d08ae3bd1f75892e046bdb23
Bohdan <ZhyzhchenkoB@gmail.com>
2023-06-19 16:22:43 +0300

    Initial Commit

---
+++ Users/UsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsersApp.swift
+//  Users
+//
+//  Created by Bohdan Zhyzhchenko on 19.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct UsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
